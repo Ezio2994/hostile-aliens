1456856e4098ecc23d91ce39fb1f48abeeadfef2
Ezio Intravaia <ezio.intravaia@hotmail.com>
2020-12-11 14:12:27 +0000

    done

--- script.js
+++ script.js
@@ -5,45 +5,51 @@ const attackShips = document.querySelectorAll(".attack article p");
 const attackShipsWhole = document.querySelectorAll(".attack article");
 
 window.addEventListener("keyup", () => {
-  if (event.keyCode === 32) {
-    shot();
-  }
+    if (event.keyCode === 32) {
+        newShot.shot();
+    }
 });
 
-const shot = () => {
-  random = Math.floor(Math.random() * 3);
-  random2 = Math.floor(Math.random() * 5);
-  random3 = Math.floor(Math.random() * 8);
+class Shot {
+    shot() {
+        let random = Math.floor(Math.random() * 3);
+        let random2 = Math.floor(Math.random() * 5);
+        let random3 = Math.floor(Math.random() * 8);
 
     if (random === 0) {
         if(Number(motherShip.innerText) > 1) {
             if (random2 > 3) {
                 motherShip.innerText = Number(motherShip.innerText) - 9;
             } else {
-                shot()
+                newShot.shot();
             }
         } else {
-            won()
+            callWin.won()
     }} else if (random === 1) {
         if (Number(defenceShips[random2].innerText) > 10) {
             defenceShips[random2].innerText = Number(defenceShips[random2].innerText) - 10;
         } else {
             defenceShipsWhole[random2].style.display = 'none';
-            shot()
-    }} else if (random === 2) {
+            newShot.shot();
+        }} else if (random === 2) {
         if (Number(attackShips[random3].innerText) > 9) {
             attackShips[random3].innerText = Number(attackShips[random3].innerText) - 12;
         } else {
             attackShipsWhole[random3].style.display = 'none';
-            shot()
-    }}
+            newShot.shot();
+        }}}
 };
 
-const won = () => {
-    alert('You won! click "enter" to play again :)')
-    motherShip.innerText= '100'
-    defenceShips.forEach(ship => ship.innerText = '80')
-    attackShips.forEach(ship => ship.innerText = '45')
-    defenceShipsWhole.forEach(ship => ship.style.display = 'block')
-    attackShipsWhole.forEach(ship => ship.style.display = 'block')
+class Won {
+    won () {
+        alert('You won! click "enter" to play again :)')
+        motherShip.innerText= '100'
+        defenceShips.forEach(ship => ship.innerText = '80')
+        attackShips.forEach(ship => ship.innerText = '45')
+        defenceShipsWhole.forEach(ship => ship.style.display = 'block')
+        attackShipsWhole.forEach(ship => ship.style.display = 'block')
+    }
 }
+
+const newShot = new Shot()
+const callWin = new Won()
